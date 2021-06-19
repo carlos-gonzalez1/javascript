2fc0de9cb783a346a75ee278d8d241558b9baeeb
“carlos-gonzalez1” <coty.1089@gmail.com>
2021-06-19 13:33:08 -0300

    javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+// primer algoritmo
+// const saludo = prompt("Ingrese nombre");
+// const espacio = " ";
+// console.log("Hola" + espacio +  saludo + "!");
+
+// segundo algoritmo
+// const perdirNumero = parseInt(prompt("Ingrese número"));
+// const numero = 30;
+// // console.log(perdirNumero + numero);
+// const suma = perdirNumero + numero;
+// console.log(suma);
+
+const edad = parseInt(prompt("Ingrese su edad"));
+alert("Mi edad es " + edad);
+const Fecha = parseInt(prompt("Ingrese su año de nacimienro"));
+alert("Mi año de nacimiento es " + Fecha);
+const Dia = parseInt(prompt("Ingrese dia"));
+alert("Mi Dia de nacimiento es " + Dia);
+const Mes = parseInt(prompt("Ingrese Mes"));
+alert("Mi mes de nacimiento es " + Mes);
+const ingreseNombre = prompt("Ingrese su nombre");
+const ingreseSuApellido = prompt("Ingrese apellido");
+alert("Bienvenido " + ingreseNombre + " " + ingreseSuApellido + "!");
