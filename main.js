08fb1b445d268daa2629c5a6a33171accae0f8c6
carlos-gonzalez1 <81539131+carlos-gonzalez1@users.noreply.github.com>
2021-06-23 16:43:48 -0300

    agregue desafió2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Desafio1
 // primer algoritmo
 // const saludo = prompt("Ingrese nombre");
 // const espacio = " ";
@@ -10,14 +11,81 @@
 // const suma = perdirNumero + numero;
 // console.log(suma);
 
-const edad = parseInt(prompt("Ingrese su edad"));
-alert("Mi edad es " + edad);
-const Fecha = parseInt(prompt("Ingrese su año de nacimienro"));
-alert("Mi año de nacimiento es " + Fecha);
-const Dia = parseInt(prompt("Ingrese dia"));
-alert("Mi Dia de nacimiento es " + Dia);
-const Mes = parseInt(prompt("Ingrese Mes"));
-alert("Mi mes de nacimiento es " + Mes);
-const ingreseNombre = prompt("Ingrese su nombre");
-const ingreseSuApellido = prompt("Ingrese apellido");
-alert("Bienvenido " + ingreseNombre + " " + ingreseSuApellido + "!");
+// const edad = parseInt(prompt("Ingrese su edad"));
+// alert("Mi edad es " + edad);
+// const Fecha = parseInt(prompt("Ingrese su año de nacimienro"));
+// alert("Mi año de nacimiento es " + Fecha);
+// const Dia = parseInt(prompt("Ingrese dia"));
+// alert("Mi Dia de nacimiento es " + Dia);
+// const Mes = parseInt(prompt("Ingrese Mes"));
+// alert("Mi mes de nacimiento es " + Mes);
+// const ingreseNombre = prompt("Ingrese su nombre");
+// const ingreseSuApellido = prompt("Ingrese apellido");
+// alert("Bienvenido " + ingreseNombre + " " + ingreseSuApellido + "!");
+
+//desafio2
+
+// const numero1 = 15;
+
+// if(numero1 == 10){
+//     console.log("verdadero!");
+// }else{
+//     console.log("falso!");
+// }
+
+// const numero2 = 10;
+// const numero3 = 11;
+
+// if(numero2 == 10 && numero3 == 11){
+//     console.log("verdadero!");
+// }else{
+//     console.log("falso!");
+// }
+
+// const nombreDeUsuario = prompt("Ingrese nombre de usuario");
+// if (nombreDeUsuario === "carlos"){
+//     console.log("Ingrese su nombre con la primera letra en mayúscula");
+// }
+// else if (nombreDeUsuario === "Carlos"){
+//     alert("bienvenidos!");
+// }
+
+// const password = 999;
+// const nombre = "carlos";
+// const apellido = "Gonzalez";
+
+// if(password == 999 && (nombre == "carlos" || apellido == "gonzalez")){
+//     console.log("bienvenidos " + nombre + " " + apellido);
+// }else{
+//     console.log("Datos incorrectos");
+// }
+
+// let precio = 8000;
+
+// if (precio == 7000){
+//     precio = 6500;
+//     console.log("se aplico un descuento ahora su precio es $" + precio);
+// }else if(precio == 8000){
+//     precio = 7500;
+//     console.log("se aplico un descuento ahora su preco es $" + precio);
+// }else if(precio == 6000){
+//     precio = 5500;
+//     console.log("se aplico un descuento ahora su precio es $" + precio);
+// }else{
+//     console.log("no hay descuentos disponible su precio es $" + precio);
+// }
+
+// const nombreIngresado =prompt("Ingresar nombre");
+// if ((nombreIngresado != "") && ((nombreIngresado == "JUAN") || (nombreIngresado == "juan"))){
+//     console.log("Hola Juan");
+// }
+// else{
+//     console.log("Error: Ingresar nombre válido");
+// }
+
+// const numero = parseInt(prompt("Ingrese numero"));
+// if (numero > 800){
+//     alert ("el numero Ingresado es: "+ numero + " y es mayor a 800");
+// }else{
+//     alert ("el numero Ingresado es: "+ numero +" y es menor a 800");
+// }
