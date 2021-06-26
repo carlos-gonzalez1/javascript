0ad229fd33790bb0f6adee5cf4926f398cb47119
carlos-gonzalez1 <81539131+carlos-gonzalez1@users.noreply.github.com>
2021-06-26 19:27:24 -0300

    desafio3

--- main.js
+++ main.js
@@ -1,91 +1,47 @@
-//Desafio1
-// primer algoritmo
-// const saludo = prompt("Ingrese nombre");
-// const espacio = " ";
-// console.log("Hola" + espacio +  saludo + "!");
+// Desafio3
+// ejemplos con for while do..while
+// for
+for (let i = 1; i <=4; i++){
+    console.log("iteracion ${i}");
+}
+console.log("Fin del ciclo");
 
-// segundo algoritmo
-// const perdirNumero = parseInt(prompt("Ingrese número"));
-// const numero = 30;
-// // console.log(perdirNumero + numero);
-// const suma = perdirNumero + numero;
-// console.log(suma);
+for (let i = 4; i >=1; i--){
+    console.log("iteracion ${i}");
+}
+console.log("Fin del ciclo");
 
-// const edad = parseInt(prompt("Ingrese su edad"));
-// alert("Mi edad es " + edad);
-// const Fecha = parseInt(prompt("Ingrese su año de nacimienro"));
-// alert("Mi año de nacimiento es " + Fecha);
-// const Dia = parseInt(prompt("Ingrese dia"));
-// alert("Mi Dia de nacimiento es " + Dia);
-// const Mes = parseInt(prompt("Ingrese Mes"));
-// alert("Mi mes de nacimiento es " + Mes);
-// const ingreseNombre = prompt("Ingrese su nombre");
-// const ingreseSuApellido = prompt("Ingrese apellido");
-// alert("Bienvenido " + ingreseNombre + " " + ingreseSuApellido + "!");
 
-//desafio2
-
-const numero1 = 15;
-
-if(numero1 == 10){
-    console.log("verdadero!");
-}else{
-    console.log("falso!");
+for (let i = 1; i <= 4; i++){
+    console.log("Empieza iteracion ${i}");
+    for (let j = 0; j < 4; j++){
+        console.log(j);
+    }
 }
 
-const numero2 = 10;
-const numero3 = 11;
 
-if(numero2 == 10 && numero3 == 11){
-    console.log("verdadero!");
-}else{
-    console.log("falso!");
-}
+let ingresarNumero = parseInt(prompt("Ingresar numero"));
+for (let i = 1; i <= 10; i++){
+        let resultado = ingresarNumero * i ;
+        console.log(ingresarNumero +" x "+ i +" = "+ resultado);
+    }
+console.log("Fin del ciclo");
 
-const nombreDeUsuario = prompt("Ingrese nombre de usuario");
-if (nombreDeUsuario === "carlos"){
-    console.log("Ingrese su nombre con la primera letra en mayúscula");
-}
-else if (nombreDeUsuario === "Carlos"){
-    alert("bienvenidos!");
-}
+//ejemplo de while
 
-const password = 999;
-const nombre = "carlos";
-const apellido = "Gonzalez";
-
-if(password == 999 && (nombre == "carlos" || apellido == "gonzalez")){
-    console.log("bienvenidos " + nombre + " " + apellido);
-}else{
-    console.log("Datos incorrectos");
+let x = 1;
+while (x <= 4){
+    console.log("Iteracion ${x}");
+    x++;
 }
+console.log("Fin de while");
 
-let precio = 8000;
+//ejemplo de do...while
 
-if (precio == 7000){
-    precio = 6500;
-    console.log("se aplico un descuento ahora su precio es $" + precio);
-}else if(precio == 8000){
-    precio = 7500;
-    console.log("se aplico un descuento ahora su preco es $" + precio);
-}else if(precio == 6000){
-    precio = 5500;
-    console.log("se aplico un descuento ahora su precio es $" + precio);
-}else{
-    console.log("no hay descuentos disponible su precio es $" + precio);
-}
+let y = 1;
 
-const nombreIngresado =prompt("Ingresar nombre");
-if ((nombreIngresado != "") && ((nombreIngresado == "JUAN") || (nombreIngresado == "juan"))){
-    console.log("Hola Juan");
-}
-else{
-    console.log("Error: Ingresar nombre válido");
-}
+do{
+    console.log("Iteracion ${y}");
+    y++;
 
-const numero = parseInt(prompt("Ingrese numero"));
-if (numero > 800){
-    alert ("el numero Ingresado es: "+ numero + " y es mayor a 800");
-}else{
-    alert ("el numero Ingresado es: "+ numero +" y es menor a 800");
-}
+}while (y <= 4);
