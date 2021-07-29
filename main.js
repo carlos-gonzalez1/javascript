c44f8cd5e06c1a178799d9f0287edfca4a1a7e65
carlos-gonzalez1 <81539131+carlos-gonzalez1@users.noreply.github.com>
2021-07-29 16:51:39 -0300

    ac js

--- main.js
+++ main.js
@@ -1,175 +1,151 @@
 //DOM , EVENTOS Y COMPLEMENTARIO
 
 class Producto {
-  constructor(marca, nombre, precio) {
-      this.marca = marca;
-      this.nombre = nombre.toUpperCase();
-      this.precio = precio;
-  }
+    constructor(upc, nombre, marca, precio) {
+        this.upc = upc;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
 }
 
-class Persona {
-  constructor(nombre) {
-      this.nombre = nombre;
-  }
+//  Evento para eliminar todo 
+const botonRemoverProductos = document.getElementById("revoverProductos")
+botonRemoverProductos.addEventListener("click", removerProductos);
 
-  agregarProducto(producto) {
-      if (this.productos.find(item => item.marca == producto.marca)) {
-          return 'Producto ya existe';
-      } else {
-          this.productos.push(producto);
-          return 'Producto agregado';
-      }
-  }
-
-  actualizarDeposito() {
-      localStorage.setItem(`${this.nombre}`, JSON.stringify(this.productos));
-  }
-
-  cargarDeposito() {
-      const deposito = JSON.parse(localStorage.getItem(`${this.nombre}`));
-      
-      if (deposito) {
-          this.productos = deposito;
-      } else {
-          this.productos = [];
-      }
-  }
-
-  mostrarProductos() {
-      const section = document.createElement("section");
-      const nombrePersona = document.createElement("h1");
-      nombrePersona.textContent = `Bienvenid@ a ${this.nombre}`;
-      section.appendChild(nombrePersona);
-
-      this.productos.map(elemento => {
-          section.appendChild(this.armarTarjeta(elemento));
-      });
-
-      document.body.appendChild(section);
-  }
-  
-  armarTarjeta(elemento) {
-      const tarjeta = document.createElement("div");
-      tarjeta.classList.add("tarjeta");
-
-      const nombreProducto = document.createElement("h3");
-      nombreProducto.textContent = `${elemento.nombre}`
-      tarjeta.appendChild(nombreProducto);
-
-      const marca = document.createElement("div");
-      marca.textContent = `Marca: ${elemento.marca}`;
-      tarjeta.appendChild(marca);
-
-      const precio = document.createElement("div");
-      precio.textContent = `Precio: ${elemento.precio}`;
-      
-    
-      elemento.precio > 8000 ? precio.classList.add("uno") : precio.classList.add("dos");
-
-      tarjeta.appendChild(precio);
-      return tarjeta;
-  }
-}
-
-const consultarIngresoDeProdcuto = () => prompt('¿Desea ingresar un producto? Y/N ').toUpperCase() === 'Y';
-
-const cargarProductos = (Persona) => {
-  while (consultarIngresoDeProdcuto()) {
-      const producto = new Producto(prompt('Marca'), prompt('Nombre del producto'), parseFloat(prompt('Precio')));
-      alert(Persona.agregarProducto(producto));
-  }
-}
-
-function iniciarPrograma() {
-  const persona = new Persona(prompt("INGRESE SU NOMBRE"));
-  persona.cargarDeposito();
-  cargarProductos(persona);
-  persona.actualizarDeposito();
-  persona.mostrarProductos();
-}
-
-iniciarPrograma();
-
-
-//evento para dark mode
-
-let botonDarkMode = document.getElementById("darkMode");
+//  Evento para dark mode
+const botonDarkMode = document.getElementById("darkMode");
 botonDarkMode.addEventListener("click", cambiarTema);
 
-//eventos para guardar un formulario
-
-let formulario = document.getElementById("formularioProductos");
-formulario.addEventListener("submit", guardarProducto);
-
-//evento para mostar menu de productos
-let mostrarMenu = document.getElementById("mostrarMenu");
+//  Evento para mostrar menu productos
+const mostrarMenu = document.getElementById("mostrarMenu");
 mostrarMenu.addEventListener("click", mostrarFormulario)
 
-//funcio para cambiar tema
+//  Evento para guardar formulario de productos
+const formulario = document.getElementById("formularioProductos-1");
+formulario.addEventListener("submit", guardarProducto);
 
+//  Funcion para cambiar tema
 function cambiarTema() {
     document.body.classList.toggle("darkMode");
 }
 
-//funcion para cargar listado del local o iniciarlo si no hay
+//  Elimniamos todos los productos
+function removerProductos() {
+    localStorage.clear();
+    mostrarListado(cargarListado());
+}
 
+//  Funcion para cargar listado del localStorage o iniciarlo si no hay
 function cargarListado() {
-    let listadoProductos = JSON.parse(localStorage.getItem("listadoProductos"));
-    if(listadoProductos == null) {
+    const listadoProductos = JSON.parse(localStorage.getItem("listadoProductos"));
+    if (listadoProductos == null) {
         return [];
     }
     return listadoProductos;
 }
 
-//funcion para guardar en localStorage
+//  Funcion para guardar en localStorage
 function guardarListado(listadoProductos) {
     localStorage.setItem("listadoProductos", JSON.stringify(listadoProductos));
     mostrarListado(listadoProductos);
 }
 
-//funcion para guardar
-
+//  Funcion para guardar producto
 function guardarProducto(e) {
     e.preventDefault();
-    let nombre = document.querySelector("#nombre").value;
-    let marca = document.querySelector("#marca").value;
-    let precio = document.querySelector("#precio").value;
+    const formulario = new FormData(e.target);
+    const upc = formulario.get("upc");
+    const nombre = formulario.get("nombre");
+    const marca = formulario.get("marca");
+    const precio = formulario.get("precio");
+    
 
-    let listadoProductos = cargarProductos();
-    listadoProductos.push(new Producto(nombre, marca, precio));
+    const producto = new Producto(upc, nombre, marca, precio);
 
-    guardarListado(listadoProductos);
-    document.getElementById('formularioProductos').reset();
+    if (validarformulario(producto)) {
+        
+        const listadoProductos = cargarListado();
+
+        listadoProductos.push(producto);
+
+        guardarListado(listadoProductos);
+
+        document.getElementById('formularioProductos-1').reset();
+    } else {
+        mostrarError("Campos vacios, por favor ingrese todos los datos");
+    }
 }
 
-//funcion para mostrar menu de productos
+//  Muestra un error (Nuevo)
+function mostrarError(mensaje) {
+    const div = document.createElement("div");
+    div.classList.add("alert", "alert-danger", "m-3");
+    div.textContent = mensaje;
+    const menu = document.getElementById("menuAgregar");
+    menu.appendChild(div);
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+//  Valida que no tenga campos vacios 
+function validarformulario(producto){
+    let salida = true;
+    const campos = Object.values(producto);
+    campos.forEach(campo => {
+        if (campo === " ") {
+            salida = false;
+        }
+    });
+    return salida;
+}
+
+//  Funcion para mostrar menu de producto
 function mostrarFormulario() {
     document.getElementById("menuAgregar").classList.toggle("oculto");
 }
 
-//funcion para armar una tarjeta 
+//  Funcion para armar una tarjeta
 function armarTarjeta(elemento) {
     const tarjeta = document.createElement("div");
-    tarjeta.classList.add("tarjeta");
+    tarjeta.classList.add("tarjeta", "m-2");
+
+    const btnEliminar = document.createElement("div");
+    btnEliminar.textContent = "Eliminar";
+    btnEliminar.classList.add("btn", "btn-danger", "float-end", "m-2");
+    btnEliminar.setAttribute("id", elemento.upc);
+    btnEliminar.setAttribute("onclick", `eliminarProducto(${elemento.upc})`);
+    tarjeta.appendChild(btnEliminar);
 
     const nombreProducto = document.createElement("h3");
-    nombreProducto.textContent = `${elemento.nombre}`
+    nombreProducto.textContent = elemento.nombre
     tarjeta.appendChild(nombreProducto);
 
     const marca = document.createElement("div");
-    marca.textContent = `marca: ${elemento.marca}`;
+    marca.textContent = `Marca: ${elemento.marca}`;
     tarjeta.appendChild(marca);
+
+    const precio = document.createElement("div");
+    precio.textContent = `Precio: ${elemento.precio}`;
+    tarjeta.appendChild(precio);
+
     return tarjeta;
 }
 
+//  Eliminar un producto especifico
+function eliminarProducto(upc){
+    let listadoProductos = cargarListado();
+    listadoProductos = listadoProductos.filter(producto => producto.upc != upc);
+    guardarListado(listadoProductos);
+}
+
 function mostrarListado(listadoProductos) {
-    let listado = document.getElementById("listado");
+    const listado = document.getElementById("listado");
     listado.textContent = "";
     listadoProductos.map(elemento => {
         listado.appendChild(armarTarjeta(elemento));
     });
 }
 
-
-mostrarListado(cargarProductos());
+mostrarListado(cargarListado());
