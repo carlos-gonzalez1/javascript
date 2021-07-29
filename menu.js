142a005d973081728108fa66da3a7f0d0e60b3dd
carlos-gonzalez1 <81539131+carlos-gonzalez1@users.noreply.github.com>
2021-07-29 17:05:11 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-//MENÚ
-
-const btnMenu = document.querySelector("#btnMenu");
-const menu = document.querySelector("#menu");
-btnMenu.addEventListener("click", function(){
-    menu.classList.toggle("mostrarMenu");
-});
-
