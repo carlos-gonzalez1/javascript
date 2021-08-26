59b21ce3451612d67b2777be27c77da9614d4c0f
carlos-gonzalez1 <81539131+carlos-gonzalez1@users.noreply.github.com>
2021-08-26 15:55:08 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,6 @@
 // menu
-const btnToggle = document.querySelector('.toggle-btn');
-
-btnToggle.addEventListener('click', function () {
-  console.log('clik')
-  document.getElementById('sidebar').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
+$(document).ready(function(){
+  $('.menu-toggle').click(function(){
+      $('nav').toggleClass('active');
+  });
 });
